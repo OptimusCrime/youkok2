7bb76a051b4609200343445d569b423445184bac
thomasgautvedt <thomasgautv@hotmail.com>
2013-10-02 01:03:26 +0200

    Added public methods returning testing-values

--- user.php
+++ user.php
@@ -8,4 +8,54 @@
  * 
 */
 
+Class User {
+
+    //
+    // The internal variables
+    //
+    
+    private $id;
+    private $name;
+    private $is_ntnu;
+    
+    //
+    // Constructor
+    //
+
+    public function __construct() {
+        // TODO
+    }
+    
+    //
+    // Returning if the user is logged in or not
+    //
+    
+    public function isLoggedIn() {
+        return 1;
+    }
+    
+    //
+    // Returning the current user-id
+    //
+    
+    public function getId() {
+        return 1;
+    }
+    
+    //
+    // Returning the current username
+    //
+    
+    public function getName() {
+        return '<i>Anonym</i>';
+    }
+    
+    //
+    // Returning if the current user is NTNU-user or not
+    //
+    
+    public function getIsNTNU() {
+        return 1;
+    }
+}
 ?>
