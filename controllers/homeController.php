a0a2c29c9fd06ffe654c69eb4e7e33388ae536ca
thomasgautvedt <thomasgautv@hotmail.com>
2013-10-02 22:07:00 +0200

    Renamed method for prettifying an url and changed some minor stuff

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -60,7 +60,7 @@ class HomeController extends Base {
         $get_newest_query->execute();
         while ($row = $get_newest_query->fetch(PDO::FETCH_ASSOC)) {
             // Build string
-            $ret .= '<li><a target="_blank" href="last-ned/'.$this->reverseUrl($row['path']).'">'.$row['name'].'</a></li>';
+            $ret .= '<li><a href="last-ned/'.$this->prettifyUrl($row['path']).'">'.$row['name'].'</a></li>';
         }
         
         return $ret;
@@ -84,7 +84,7 @@ class HomeController extends Base {
         $get_most_popular_query->execute();
         while ($row = $get_most_popular_query->fetch(PDO::FETCH_ASSOC)) {
             // Build string
-            $ret .= '<li><a target="_blank" href="last-ned/'.$this->reverseUrl($row['path']).'">'.$row['name'].' (lastet ned: '.number_format($row['downloaded_times']).' ganger)</a></li>';
+            $ret .= '<li><a href="last-ned/'.$this->prettifyUrl($row['path']).'">'.$row['name'].' (lastet ned: '.number_format($row['downloaded_times']).' ganger)</a></li>';
         }
         
         return $ret;
