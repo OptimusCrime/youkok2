8f051adce5a0c294fe650f8927f3c5f3000e4662
thomasgautvedt <thomasgautv@hotmail.com>
2013-10-02 02:04:27 +0200

    Made reverse url-lookup and url-friendly method

--- base.php
+++ base.php
@@ -51,6 +51,85 @@ class Base {
     }
     
     //
+    // Makes reverse url-lookup
+    //
+    
+    protected function reverseUrl($url) {
+        // Checking if we got got an empty url
+        if (strlen($url) == 0) {
+            return '';
+        }
+        
+        // Checking if roo
+        if ($url == '/') {
+            return '/';
+        }
+        
+        // Check if multiple levels deep
+        $url_pieces_temp = array();
+        if (strpos($url,'/') !== false) {
+            // Multiple levels
+            $url_pieces_temp = explode('/',$url);
+        }
+        else {
+            // Single level
+            $url_pieces_temp[] = $url;
+        }
+        
+        // Remove all empty pieces
+        $url_pieces = array();
+        for ($i = 0; $i < count($url_pieces_temp); $i++) {
+            if (strlen($url_pieces_temp[$i]) > 0) {
+                $url_pieces[] = '/'.$url_pieces_temp[$i];
+            }
+        }
+        
+        // Check if we have just root left now
+        if (count($url_pieces) == 1 and $url_pieces[0] == '/') {
+            return '/';
+        }
+        
+        // Build correct paths
+        $temp = '';
+        for ($i = 0; $i < count($url_pieces); $i++) {
+            $temp .= $url_pieces[$i];
+            $url_pieces[$i] = $temp;
+        }
+        
+        // Now build the correct string
+        $ret = '';
+        foreach ($url_pieces as $path) {
+            $get_revese_url = "SELECT name, path
+            FROM archive 
+            WHERE path = :path";
+            
+            $get_revese_url_query = $this->db->prepare($get_revese_url);
+            $get_revese_url_query->execute(array(':path' => $path));
+            $row = $get_revese_url_query->fetch(PDO::FETCH_ASSOC);
+            
+            $ret .= '/'.$row['name'];
+        }
+        
+        // Turn url-friendly and return
+        return $this->frienlyUrl($ret);
+    }
+    
+    //
+    // Build url-friendly-url
+    //
+    
+    protected function frienlyUrl($url) {
+        // Turn all lowercase
+        $url = strtolower($url);
+        
+        // Replace space with -
+        $url = str_replace(' ','-',$url);
+        
+        // Return all
+        return $url;
+    }
+    
+    //
     // Close the database-connection and clean up before displaying the template
     //
     
