758d7ee61a0381076a750ae97c2a384bdf0141f1
thomasgautvedt <thomasgautv@hotmail.com>
2013-10-02 22:06:41 +0200

    WIP downloading for a single file

--- controllers/downloadController.php
+++ controllers/downloadController.php
@@ -22,9 +22,71 @@ class DownloadController extends Base {
         // Calling Base' constructor
         parent::__construct();
         
-        echo '<h1>Downloading...</h1>';
+        // Parse the correct path
+        $file_location = $this->getFileRealLocation();
+        
+        // Check if it returned a real file or not
+        if (!$file_location) {
+            // Did not return a real file
+            $this->return404();
+        }
+        else {
+            // Did return a real file
+        }
         
         // http://php.net/manual/en/function.readfile.php
+    }
+    
+    //
+    //
+    //
+    
+    private function getFileRealLocation() {
+        // Getting the path
+        if (!isset($_GET['q'])) {
+            return false;
+        }
+        else {
+            // Reverse the url
+            $file = $this->reverseUrl($_GET['q']);
+            
+            // Check if the file exists or not
+            if (1 == 1) {
+                // File exists, buffer and load the file
+                $this->loadFile($file);
+            }
+            else {
+                // File does not exists, log error and return 404
+                $this->return404();
+            }
+        }
+    }
+    
+    //
+    //
+    //
+    
+    private function return404() {
+        return '<h1>File not found...</h1>';
+    }
+    
+    //
+    //
+    //
+    
+    private function loadFile($file) {
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename='.basename($file));
+        header('Content-Transfer-Encoding: binary');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($file));
+        ob_clean();
+        flush();
+        readfile($file);
+        exit;
     }
 }
 
