cb46da905a1d1049b257a236d679820ff98ef7bc
OptimusCrime <thomasgautv@hotmail.com>
2014-04-22 02:02:44 +0200

    Forgot the supid dates again

--- base.php
+++ base.php
@@ -3,7 +3,7 @@
  * File: rest.php
  * Holds: The base-class intilize most of the common stuff the system needs
  * Created: 02.10.13
- * Last updated: 18.04.14
+ * Last updated: 22.04.14
  * Project: Youkok2
  * 
 */
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * File: index.php
  * Holds: The Loader class that loads the correct class based on the method being called, setting output and including all the stuff we need
  * Created: 02.10.13
- * Last updated: 17.04.14
+ * Last updated: 22.04.14
  * Project: Youkok2
  * 
 */
