5d3017e25343a72f9fec090d08c62e716b7e5956
OptimusCrime <thomasgautv@hotmail.com>
2014-05-02 20:09:16 +0200

    Began writing fix for localstoreage and typeahead

---
+++ cache-example.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * File: cache-example.php
+ * Holds: Cache
+ * Created: 02.05.14
+ * Last updated: 02.05.14
+ * Project: Youkok2
+ * 
+*/
+
+// Copy this file and call it cache. Will hold the cache-values for typeahead and other stuff
+?>
