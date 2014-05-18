4a289d809afb59138ad03d07ee33e3ba54d17946
OptimusCrime <thomasgautv@hotmail.com>
2014-05-18 16:56:27 +0200

    Added upgrade-script

---
+++ upgrade.sh
@@ -0,0 +1,19 @@
+#
+# File: upgrade.sh
+# Holds: File to run for upgrading the system
+# Created: 18.05.14
+# Last updated: 18.05.14
+# Project: Youkok2
+#
+
+#
+# Pull
+#
+
+git pull origin master
+
+#
+# Fetch
+#
+
+git fetch origin master
