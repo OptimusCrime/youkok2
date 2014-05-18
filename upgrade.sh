2628a5bf014de14b0e32080c22c88b466afd5823
OptimusCrime <thomasgautv@hotmail.com>
2014-05-18 17:13:08 +0200

    Fucked up deployment script

--- upgrade.sh
+++ upgrade.sh
@@ -1,19 +1,3 @@
-#
-# File: upgrade.sh
-# Holds: File to run for upgrading the system
-# Created: 18.05.14
-# Last updated: 18.05.14
-# Project: Youkok2
-#
-
-#
-# Pull
-#
-
-git pull origin master
-
-#
-# Fetch
-#
-
-git fetch origin master
+﻿#!/bin/bash
+echo "git pull"
+echo "git fetch origin master"
