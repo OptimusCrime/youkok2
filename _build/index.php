8944ce030b8ab90bb82ea21916334b7fba1106b2
OptimusCrime <thomasgautv@hotmail.com>
2014-07-13 19:57:18 +0200

    Began implementing linecounter script

--- _build/index.php
+++ _build/index.php
@@ -12,15 +12,18 @@
 //
 
 require_once '../local.php';
+
 require_once BASE_PATH . '/_build/libs/minify/Minify.php';
 require_once BASE_PATH . '/_build/libs/minify/CSS.php';
 require_once BASE_PATH . '/_build/libs/minify/JS.php';
 require_once BASE_PATH . '/_build/libs/minify/Exception.php';
 
+require_once BASE_PATH . '/_build/libs/youkok2/linecounter.class.php';
+
 //
 // Set namespace
 //
-
+/*
 use MatthiasMullie\Minify;
 
 //
@@ -39,6 +42,45 @@ echo '<span style="color: green;">Minified CSS</span><br />';
 $js_minifier = new Minify\JS(BASE_PATH . '/assets/js/youkok.js');
 $js_minifier->minify(BASE_PATH . '/assets/js/youkok.min.js');
 echo '<span style="color: green;">Minified JS</span><br />';
+*/
+//
+// Count number of lines
+//
+
+$ignore_paths = array(
+	// Remove all directories we don't want
+	'!.git/',
+	'!_build/',
+	'!assets/',
+	'!cache/',
+	'!files',
+	'!libs/',
+	'!processor/',
+	'!templates_c/',
+	'!vendor/',
+);
+
+$ignore_files = array(
+	// Remove all root files we don't want
+	'!.htaccess',
+	'!.gitignore',
+	'!local.php',
+	'!composer.lock',
+);
+
+$add_files = array(
+	// Override directories we have ignored earlier
+	'_build/libs/youkok2/index.php',
+	'_build/libs/youkok2/libs/youkok2/linecounter.class.php',
+	'assets/js/youkok.js',
+	'assets/css/youkok.css',
+	'libs/youkok2/cachemanager.php',
+	'libs/youkok2/executioner.php',
+	'libs/youkok2/utilities.php',
+);
+
+$linecounter = new LineCounter($ignore_paths, $ignore_files, $add_files);
+$linecounter->analyze();
 
 //
 // Finish
---
+++ _build/libs/youkok2/linecounter.class.php
@@ -0,0 +1,173 @@
+<?php
+
+Class LineCounter {
+
+	private $ignore_paths;
+	private $ignore_files;
+	private $add_files;
+
+	private $elements;
+	private $lines;
+	private $lines_type;
+
+	public function __construct($ignore_paths, $ignore_files, $add_files) {
+		$this->ignore_paths = $ignore_paths;
+		$this->ignore_files = $ignore_files;
+		$this->add_files = $add_files;
+
+		$this->elements = array('/' => array());
+		$this->lines = array();
+		$this->lines_type = array();
+	}
+
+	public function analyze() {
+
+		//
+		// Fix ignore paths
+		//
+
+		$this->ingorePaths();
+
+		//
+		// Add files
+		//
+
+		$this->addFiles();
+
+		//
+		// Number of lines
+		//
+
+		$this->lines('', $this->elements);
+
+		echo "<pre>";
+		print_r($this->lines);
+		echo "<hr />";
+		print_r($this->lines_type);
+		echo "<hr />";
+		echo array_sum($this->lines_type);
+	}
+
+	private function ingorePaths ($dir = '/') {
+		if ($handle = opendir(BASE_PATH . $dir)) {
+			while (false !== ($entry = readdir($handle))) {
+				if ($entry != '..' and $entry != '.' and $entry != '.DS_Store' and $entry != '._.DS_Store') {
+					if (is_dir(BASE_PATH . $dir . $entry)) {
+						if (!$this->isIgnored($dir, $entry . '/')) {
+							$this->ingorePaths($dir . $entry . '/');
+						}
+					}
+					else {
+						if (!$this->isIgnored($dir, $entry, false)) {
+							$node = $this->getNode($dir, $entry);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	private function addFiles() {
+		foreach ($this->add_files as $v) {
+			$v_split = explode('/', $v);
+			$current = '/';
+			foreach ($v_split as $ik => $iv) {
+				if (count($v_split) == ($ik + 1)) {
+					$this->getNode($current, $iv);
+				}
+				else {
+					$current .= $iv . '/';
+				}
+				
+			}
+		}
+	}
+
+	private function isIgnored($dir, $file, $is_dir = true) {
+		if ($dir == '/') {
+			if ($is_dir) {
+				foreach ($this->ignore_paths as $v) {
+					if ($v == '!' . $file) {
+						return true;
+					}
+				}
+			}
+			else {
+				foreach ($this->ignore_files as $v) {
+					if ($v == '!' . $file) {
+						return true;
+					}
+				}
+			}	
+		}
+
+		return false;
+
+	}
+
+	private function getNode($dir, $entry) {
+		$s = explode('/', $dir . $entry);
+		$node = &$this->elements;
+
+		foreach ($s as $k => $v) {
+			if ($k == 0) {
+				$v = '/';
+			}
+
+			if (!isset($node[$v])) {
+				if ($v == $entry) {
+					$node[] = $v;
+					break;
+				}
+				else {
+					$node[$v] = array();
+				}
+				
+			}
+
+			$node = &$node[$v];
+		}
+		return $node;
+	}
+
+	private function lines($base, $path) {
+		$prev = '';
+		foreach ($path as $k => $v) {
+			if (is_array($v)) {
+				if ($k != '/') {
+					$prev .= $k . '/';
+				}
+				
+				$this->lines($base . $prev, $v);
+				$prev = '';
+
+			}
+			else {
+				if (is_file(BASE_PATH . '/' . $base . $v)) {
+					$linecount = 0;
+					$handle = fopen(BASE_PATH . '/' . $base . $v, "r");
+					while (!feof($handle)){
+						$line = fgets($handle);
+						$linecount++;
+					}
+
+					fclose($handle);
+					
+					$this->lines[$v] = $linecount;
+
+					$file_explode = explode('.', $v);
+					$file_type = $file_explode[count($file_explode) -1 ];
+					if (!isset($this->lines_type[$file_type])) {
+						$this->lines_type[$file_type] = $linecount;
+					}
+					else {
+						$this->lines_type[$file_type] += $linecount;
+					}
+				}
+			}
+		}
+	}
+
+}
+
+?>
