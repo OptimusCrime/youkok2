6edf88417bce41c3d9c4dd065e6be709f28f6c51
OptimusCrime <thomasgautv@hotmail.com>
2014-07-14 01:29:40 +0200

    Began working on upgrade-script

--- _upgrade/upgrade.sh
+++ _upgrade/upgrade.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-echo "Trying something new"
+echo -e "\033[0;33mExecuting git pull\033[0m \n"
 git pull --progress
+
+echo -e "\n\033[0;33mExecuting git fetch\033[0m \n"
+git fetch --progress
+
+echo -e "\n\033[0;33mClearing cache\033[0m \n"
+php "clearcache.class.php"
+
+echo -e "\n\033[0;33mRunning migrations\033[0m \n"
+php "migrate.class.php"
