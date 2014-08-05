b767ff55b54a91435cba951e31e45ba2209fb0ff
OptimusCrime <thomasgautv@hotmail.com>
2014-08-06 01:35:58 +0200

    Fixed some security to hide deleted files from the frontpage

--- controllers/home.controller.php
+++ controllers/home.controller.php
@@ -57,6 +57,7 @@ class HomeController extends Youkok2 {
         $get_newest = "SELECT id
         FROM archive
         WHERE is_directory = 0
+        AND is_visible = 1
         ORDER BY added DESC
         LIMIT 15";
         
@@ -81,7 +82,7 @@ class HomeController extends Youkok2 {
                 $local_dir_str = '';
                 if ($element->getParent() != $root_parent->getId()) {
                     $local_dir_element = $this->collection->get($element->getParent());
-                    $local_dir_str = '<a href="' . $local_dir_element->generateUrl($this->routes['archive'][0]) . '">' . $local_dir_element->getName() . '</a>, ';
+                    $local_dir_str = '<a href="' . $local_dir_element->generateUrl($this->routes['archive'][0]) . '">' . $local_dir_element->getName() . '</a> i ';
                 }
                 
                 $ret .= '<li class="list-group-item"><a rel="nofollow" href="' . $element_url . '">' . $element->getName() . '</a> @ ' . $local_dir_str . ($root_parent == null ? '' : '<a href="' . $root_parent->generateUrl($this->routes['archive'][0]) . '" data-toggle="tooltip" data-placement="top" title="' . $root_parent->getCourse()->getName() . '">' . $root_parent->getName() . '</a>') . ' [<span class="moment-timestamp" style="cursor: help;" title="' . $this->utils->prettifySQLDate($element->getAdded()) . '" data-ts="' . $element->getAdded() . '">Laster...</span>]</li>';
@@ -100,11 +101,11 @@ class HomeController extends Youkok2 {
         $ret = '';
 
         // Deltas
-        $delta = array(' WHERE d.downloaded_time >= DATE_SUB(NOW(), INTERVAL 1 WEEK)', 
-            ' WHERE d.downloaded_time >= DATE_SUB(NOW(), INTERVAL 1 MONTH)', 
-            ' WHERE d.downloaded_time >= DATE_SUB(NOW(), INTERVAL 1 YEAR)', 
-            '',
-            ' WHERE d.downloaded_time >= DATE_SUB(NOW(), INTERVAL 1 DAY)');
+        $delta = array(' WHERE d.downloaded_time >= DATE_SUB(NOW(), INTERVAL 1 WEEK) AND a.is_visible = 1', 
+            ' WHERE d.downloaded_time >= DATE_SUB(NOW(), INTERVAL 1 MONTH) AND a.is_visible = 1', 
+            ' WHERE d.downloaded_time >= DATE_SUB(NOW(), INTERVAL 1 YEAR) AND a.is_visible = 1', 
+            ' WHERE a.is_visible = 1',
+            ' WHERE d.downloaded_time >= DATE_SUB(NOW(), INTERVAL 1 DAY) AND a.is_visible = 1');
 
         if ($override == null) {
             if ($this->user->isLoggedIn()) {
@@ -129,6 +130,7 @@ class HomeController extends Youkok2 {
         // Load most popular files from the system
         $get_most_popular = "SELECT d.file as 'id', COUNT(d.id) as 'downloaded_times'
         FROM download d
+        LEFT JOIN archive AS a ON a.id = d.file
         " . $delta[$user_delta] . "
         GROUP BY d.file
         ORDER BY downloaded_times DESC
@@ -160,7 +162,7 @@ class HomeController extends Youkok2 {
                 $local_dir_str = '';
                 if ($element->getParent() != $root_parent->getId()) {
                     $local_dir_element = $this->collection->get($element->getParent());
-                    $local_dir_str = '<a href="' . $local_dir_element->generateUrl($this->routes['archive'][0]) . '">' . $local_dir_element->getName() . '</a>, ';
+                    $local_dir_str = '<a href="' . $local_dir_element->generateUrl($this->routes['archive'][0]) . '">' . $local_dir_element->getName() . '</a> i ';
                 }
                 
                 $ret .= '<li class="list-group-item"><a rel="nofollow" href="' . $element_url . '">' . $element->getName() . '</a> @ ' . $local_dir_str . ($root_parent == null ? '' : '<a href="' . $root_parent->generateUrl($this->routes['archive'][0]) . '" data-toggle="tooltip" data-placement="top" title="' . $root_parent->getCourse()->getName() . '">' . $root_parent->getName() . '</a>') . ' [' . number_format($element->getDownloadCount($user_delta)) . ']</li>';
@@ -184,10 +186,12 @@ class HomeController extends Youkok2 {
         $ret = '';
         
         // Load all favorites
-        $get_favorites = "SELECT file
-        FROM favorite
-        WHERE user = :user
-        ORDER BY ordering ASC";
+        $get_favorites = "SELECT f.file
+        FROM favorite AS f
+        LEFT JOIN archive AS a ON a.id = f.file
+        WHERE f.user = :user
+        AND a.is_visible = 1
+        ORDER BY f.ordering ASC";
         
         $get_favorites_query = $this->db->prepare($get_favorites);
         $get_favorites_query->execute(array(':user' => $this->user->getId()));
@@ -208,14 +212,19 @@ class HomeController extends Youkok2 {
                 $element_url = $element->generateUrl($this->routes['download'][0]);
                 $root_parent = $element->getRootParent();
                 if ($element->isDirectory()) {
-                    $ret .= '<li class="list-group-item"><a href="' . $element->generateUrl($this->routes['archive'][0]) . '">' . $element->getName() . '</a>' . (($root_parent == null or $element->getId() == $root_parent->getId()) ? '' : ' @ <a href="' . $root_parent->generateUrl($this->routes['archive'][0]) . '" data-toggle="tooltip" data-placement="top" title="' . $root_parent->getCourse()->getName() . '">' . $root_parent->getName() . '</a>') . '</li>';
+                    if ($element->hasCourse()) {
+                        $ret .= '<li class="list-group-item"><a href="' . $element->generateUrl($this->routes['archive'][0]) . '" data-toggle="tooltip" data-placement="top" title="' . $element->getCourse()->getName() . '">' . $element->getName() . '</a></li>';
+                    }
+                    else {
+                        $ret .= '<li class="list-group-item"><a href="' . $element->generateUrl($this->routes['archive'][0]) . '">' . $element->getName() . '</a>' . (($root_parent == null or $element->getId() == $root_parent->getId()) ? '' : ' @ <a href="' . $root_parent->generateUrl($this->routes['archive'][0]) . '" data-toggle="tooltip" data-placement="top" title="' . $root_parent->getCourse()->getName() . '">' . $root_parent->getName() . '</a>') . '</li>';
+                    }
                 }
                 else {
                     // Check if we should load local dir for element
                     $local_dir_str = '';
                     if ($element->getParent() != $root_parent->getId()) {
                         $local_dir_element = $this->collection->get($element->getParent());
-                        $local_dir_str = '<a href="' . $local_dir_element->generateUrl($this->routes['archive'][0]) . '">' . $local_dir_element->getName() . '</a>, ';
+                        $local_dir_str = '<a href="' . $local_dir_element->generateUrl($this->routes['archive'][0]) . '">' . $local_dir_element->getName() . '</a> i ';
                     }
                 
                     $ret .= '<li class="list-group-item"><a href="' . $element->generateUrl($this->routes['download'][0]) . '">' . $element->getName() . '</a> @ ' . $local_dir_str . ($root_parent == null ? '' : '<a href="' . $root_parent->generateUrl($this->routes['archive'][0]) . '" data-toggle="tooltip" data-placement="top" title="' . $root_parent->getCourse()->getName() . '">' . $root_parent->getName() . '</a>') . '</li>';
@@ -241,9 +250,11 @@ class HomeController extends Youkok2 {
         $ret = '';
         
         // Load all favorites
-        $get_last_downloads = "SELECT file
-        FROM download d
-        WHERE user = :user
+        $get_last_downloads = "SELECT d.file
+        FROM download AS d
+        LEFT JOIN archive AS a ON a.id = d.file
+        WHERE d.user = :user
+        AND a.is_visible = 1
         AND d.id = (
             SELECT dd.id
             FROM download dd
