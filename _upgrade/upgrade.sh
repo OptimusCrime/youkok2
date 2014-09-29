6d72e4ae7afec7b66716758241ee3aae5ea2877d
OptimusCrime <thomasgautv@hotmail.com>
2014-09-29 02:23:23 +0200

    Added self update of composer in upgrade script

--- _upgrade/upgrade.sh
+++ _upgrade/upgrade.sh
@@ -10,6 +10,9 @@ echo "\n\033[0;33mClearing cache\033[0m \n"
 php "_upgrade/clear_cache.php"
 
 echo "\n\033[0;33mUpdating composer\033[0m \n"
+/usr/local/bin/composer self-update
+
+echo "\n\033[0;33mUpdating composer packages\033[0m \n"
 composer update
 
 echo "\n\033[0;33mRunning migrations\033[0m \n"
