056a0d2bdbdb28fb0a972c5ce64e52e9224b379e
OptimusCrime <thomasgautv@hotmail.com>
2014-11-19 16:40:46 +0100

    Supressed some notices

--- local-default.php
+++ local-default.php
@@ -13,48 +13,48 @@ namespace Youkok2;
 */
 
 // Version
-define('VERSION', '1.0.0-dev');
-define('DEV', false);
+@define('VERSION', '1.0.0-dev');
+@define('DEV', false);
 
 // Base path
-define('BASE_PATH', dirname(__FILE__));
+@define('BASE_PATH', dirname(__FILE__));
 
 // File path
-define('FILE_PATH', BASE_PATH . '/path-to-file-root');
+@define('FILE_PATH', BASE_PATH . '/path-to-file-root');
 
 // Cache path
-define('CACHE_PATH', BASE_PATH . '/cache');
+@define('CACHE_PATH', BASE_PATH . '/cache');
 
 // Database
-define('DATABASE_CONNECTION', 'mysql:host=localhost;dbname=youkok2');
-define('DATABASE_USER', 'root');
-define('DATABASE_PASSWORD', 'password');
+@define('DATABASE_CONNECTION', 'mysql:host=localhost;dbname=youkok2');
+@define('DATABASE_USER', 'root');
+@define('DATABASE_PASSWORD', 'password');
 
 // Errors
-define('ERROR_MODE', E_ERROR);
-define('ERROR_DISPLAY', 0);
+@define('ERROR_MODE', E_ERROR);
+@define('ERROR_DISPLAY', 0);
 
 // Timezone
-define('TIMEZONE', 'Europe/London');
+@define('TIMEZONE', 'Europe/London');
 
 // Define domain
-define('DOMAIN', 'my-site.tld');
+@define('DOMAIN', 'my-site.tld');
 
 // Site url (no trailing slash)
-define('URL', 'http://my-site.tld');
+@define('URL', 'http://my-site.tld');
 
 // Full site url (trailing slash)
-define('URL_FULL', 'http://my-site.tld');
+@define('URL_FULL', 'http://my-site.tld');
 
 // Relative position (trailing slash both before and after, or just / if root)
-define('URL_RELATIVE', '/');
+@define('URL_RELATIVE', '/');
 
 // Accepted filetypes
-define('ACCEPTED_FILETYPES', 'application/pdf,text/plain,text/x-java-source,application/x-python,text/html');
-define('ACCEPTED_FILEENDINGS', 'pdf,txt,java,py,html,htm,sql,zip');
+@define('ACCEPTED_FILETYPES', 'application/pdf,text/plain,text/x-java-source,application/x-python,text/html');
+@define('ACCEPTED_FILEENDINGS', 'pdf,txt,java,py,html,htm,sql,zip');
 
 // Google analytics
-define('USE_GA', false);
+@define('USE_GA', false);
 
 // Email
-define('EMAIL_CONTACT', 'post@my-site.tld');
+@define('EMAIL_CONTACT', 'post@my-site.tld');
