a84b2f6b7663bcdf4a8aeb64a84f740c46afa4d7
OptimusCrime <thomasgautv@hotmail.com>
2014-11-19 16:59:02 +0100

    Added dummy test

---
+++ tests/Tests/Utilities/UtilitiesTest.php
@@ -0,0 +1,9 @@
+<?php
+
+class UtilitiesTest extends PHPUnit_Framework_TestCase {
+    
+    public function testDerp() {
+        $stack = array();
+        $this->assertEquals(0, count($stack));
+    }
+}
