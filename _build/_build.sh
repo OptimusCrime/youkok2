bf24d9c2a68538af1860d01da91b9f6d81d6f371
OptimusCrime <thomasgautv@hotmail.com>
2014-12-06 00:11:36 +0100

    Wiped all old migrations

--- _build/_build.sh
+++ _build/_build.sh
@@ -1,2 +1,12 @@
+echo "Wiping test database"
+echo "Todo"
+
+echo ""
+
+echo "Building database"
+php vendor/bin/phinx migrate -e test
+
+echo ""
+
 echo "Running tests"
 vendor/bin/phpunit -c tests
