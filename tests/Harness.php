b43c6ea1eb6440cb1ef62f78787b3caced2bf385
OptimusCrime <thomasgautv@hotmail.com>
2014-12-06 01:53:44 +0100

    Build test script

--- tests/Harness.php
+++ tests/Harness.php
@@ -38,7 +38,19 @@ class Harness {
         
         // Database
         $this->databaseConnect();
-        //$this->databasePopulate();
+        $this->clearDatabase();
+        
+        // Migrations
+        $this->migrateDatabase();
+        
+        // Finish
+        echo "\n";
+        echo "\033[32m╔═══════════════════════════════════════════════════════════════════╗\033[0m\n";
+        echo "\033[32m║                                                                   ║\033[0m\n";
+        echo "\033[32m║                            Running tests                          ║\033[0m\n";
+        echo "\033[32m║                                                                   ║\033[0m\n";
+        echo "\033[32m╚═══════════════════════════════════════════════════════════════════╝\033[0m\n";
+        echo "\n\n";
     }
     
     /*
@@ -46,6 +58,15 @@ class Harness {
      */
     
     private function createDirectories() {
+        // Output
+        echo "\n";
+        echo "\033[32m╔═══════════════════════════════════════════════════════════════════╗\033[0m\n";
+        echo "\033[32m║                                                                   ║\033[0m\n";
+        echo "\033[32m║                         Creating directories                      ║\033[0m\n";
+        echo "\033[32m║                                                                   ║\033[0m\n";
+        echo "\033[32m╚═══════════════════════════════════════════════════════════════════╝\033[0m\n";
+        echo "\n\n";
+        
         // Create cache directory
         if (!is_dir(CACHE_PATH)) {
             mkdir(CACHE_PATH);
@@ -61,25 +82,57 @@ class Harness {
     }
     
     /*
-     * Populate the database
+     * Clear the entire database
      */
     
-    private function databasePopulate() {
-        // Check if there is a database file
-        $db_dump = FILE_PATH . '/db.sql';
-        if (file_exists($db_dump)) {
-            // Create dummy database
-            $content = file_get_contents($db_dump);
+    private function clearDatabase() {
+        // Output
+        echo "\033[32m╔═══════════════════════════════════════════════════════════════════╗\033[0m\n";
+        echo "\033[32m║                                                                   ║\033[0m\n";
+        echo "\033[32m║                        Clearing test database                     ║\033[0m\n";
+        echo "\033[32m║                                                                   ║\033[0m\n";
+        echo "\033[32m╚═══════════════════════════════════════════════════════════════════╝\033[0m\n";
+        echo "\n\n";
+        
+        while (true) {
+            $get_all_tables = "SHOW TABLES
+            FROM youkok2_tests";
             
-            // Run queries
-            Database::$db->query($content);
+            $get_all_tables_query = Database::$db->query($get_all_tables);
+            $tables = $get_all_tables_query->fetchAll();
+            
+            // Check if database is empty now
+            if (count($tables) == 0) {
+                break;
+            }
+            
+            // Database is not empty, clear one more
+            foreach ($tables as $k => $v) {
+                $drop_table = 'DROP TABLE ' . $v[0];
+                $drop_table_query = Database::$db->exec($drop_table);
+            }
         }
-        else {
-            // Missing dump
-            echo "Missing database dump";
-            
-            // Kill
-            die();
+    }
+    
+    /*
+     * Run migrations on test database
+     */
+    
+    private function migrateDatabase() {
+        // Output
+        echo "\033[32m╔═══════════════════════════════════════════════════════════════════╗\033[0m\n";
+        echo "\033[32m║                                                                   ║\033[0m\n";
+        echo "\033[32m║              Running migrations on test database                  ║\033[0m\n";
+        echo "\033[32m║                                                                   ║\033[0m\n";
+        echo "\033[32m╚═══════════════════════════════════════════════════════════════════╝\033[0m\n";
+        echo "\n\n";
+        
+        // Running command
+        exec('php vendor/bin/phinx migrate -e test', $output);
+        
+        // Outputting to console
+        foreach ($output as $v) {
+            echo $v . "\n";
         }
     }
 }
