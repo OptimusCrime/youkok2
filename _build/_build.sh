77a543221c4e931840004e3e22ef78882ab9cb30
OptimusCrime <thomasgautv@hotmail.com>
2015-01-21 00:23:20 +0100

    Fixed build script

--- _build/_build.sh
+++ _build/_build.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+echo "Enter checksum"
+read CHECKSUM
+
+echo "\033[0;33mBuilding CSS and JS files\033[0m \n"
+php cli/cmd.php tasks/build $CHECKSUM
+
+echo "\033[0;33mRunning tests\033[0m \n"
 php vendor/bin/phpunit -c tests
