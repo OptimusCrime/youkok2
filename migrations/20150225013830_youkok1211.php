e16f53bfc1556033c302471ceb34d9e321c52274
OptimusCrime <thomasgautv@hotmail.com>
2015-02-25 02:40:18 +0100

    Migrating for removing the ordering field @ favorite

---
+++ migrations/20150225013830_youkok1211.php
@@ -0,0 +1,37 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class Youkok1211 extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     *
+     * Uncomment this method if you would like to use it.
+     *
+    public function change()
+    {
+    }
+    */
+    
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $this->table('favorite')
+             ->removeColumn('ordering')
+             ->update();
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+
+    }
+}
