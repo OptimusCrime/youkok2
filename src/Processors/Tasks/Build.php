5de190e74b6660c184c3a9e5fd17f22b8ec7b8ba
OptimusCrime <thomasgautv@hotmail.com>
2015-06-03 01:17:52 +0200

    Build derp

--- src/Processors/Tasks/Build.php
+++ src/Processors/Tasks/Build.php
@@ -31,12 +31,8 @@ class Build extends Base {
         
         // Check permissions
         if (self::requireCli() or self::requireAdmin()) {
-            // Build stuff
-            $this->buildJS();
-            //$this->buildCSS();
-            
             // Run LineCounter
-            //$this->runLineCounter();
+            $this->runLineCounter();
         }
         else {
             // No access
@@ -50,64 +46,6 @@ class Build extends Base {
         if ($this->returnData) {
             return $this->returnData();
         }
-    }
-
-    /*
-     * Build the JS files
-     */
-    
-    private function buildJS() {
-        // New instance
-        $minifier = new \MatthiasMullie\Minify\JS();
-        
-        // Add all module files
-        if ($dh = opendir(BASE_PATH . '/assets/js/youkok/')) {
-            $js_modules = '';
-            while (($file = readdir($dh)) !== false) {
-                if ($file != '.' and $file != '..') {
-                    echo BASE_PATH . '/assets/js/youkok/' . $file . '<br />';
-                    $minifier->add(BASE_PATH . '/assets/js/youkok/' . $file);
-                }
-            }
-            closedir($dh);
-        }
-        
-        // Minify!
-        $minifier->minify(BASE_PATH . '/assets/js/youkok.min.temp.js');
-        
-        // Put content on one line
-        $minified_sigle_line = '';
-        $handle = fopen(BASE_PATH . '/assets/js/youkok.min.temp.js', 'r');
-        if ($handle) {
-            while (($line = fgets($handle)) !== false) {
-                $minified_sigle_line .= rtrim($line);
-            }
-        }
-        fclose($handle);
-        
-        file_put_contents(BASE_PATH . '/assets/js/youkok.min.js', $minified_sigle_line);
-        
-        // Add message
-        $this->setData('msg', 'Built JS files');
-    }
-    
-    /*
-     * Build the CSS files
-     */
-    
-    private function buildCSS() {
-        // New instance
-        $minifier = new \MatthiasMullie\Minify\CSS();
-        
-        // Add all files
-        $minifier->add(BASE_PATH . '/assets/css/libs/bootstrap.lumen.min.css');
-        $minifier->add(BASE_PATH . '/assets/css/youkok.css');
-        
-        // Minify!
-        $minifier->minify(BASE_PATH . '/assets/css/youkok.min.css');
-        
-        // Add message
-        $this->setData('msg', 'Built CSS files');
     }
     
     /*
