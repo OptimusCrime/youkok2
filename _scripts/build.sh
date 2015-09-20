9155755709ad9e55b58a6645a65d92434605d332
OptimusCrime <thomasgautv@hotmail.com>
2015-09-20 06:03:32 +0200

    Add cloc to build script

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -3,3 +3,15 @@
 # Run tests
 echo "\033[0;33mRunning tests\033[0m \n"
 php vendor/bin/phpunit -c tests
+
+# Newline
+echo ""
+
+# Check if we can run cloc
+if hash cloc 2>/dev/null; then
+    # We can run cloc
+    echo "\033[0;33mCounting lines with cloc\033[0m \n"
+    cloc --exclude-dir=cache,node_modules,vendor ./
+else
+    echo "\033[0;33mcloc not installed\033[0m \n"
+fi
