1da9865a8c5a3f32f3b973eb4fdee1e96173a092
OptimusCrime <thomasgautv@hotmail.com>
2015-12-01 00:37:53 +0100

    Fix incorrect sum of course download numbers

--- src/Jobs/CourseDownloadUpdater.php
+++ src/Jobs/CourseDownloadUpdater.php
@@ -8,6 +8,7 @@
 
 namespace Youkok2\Jobs;
 
+use \Youkok2\Models\Element as Element;
 use \Youkok2\Models\CourseDownloads as CourseDownloads;
 use \Youkok2\Models\Controllers\CourseDownloadsController as CourseDownloadsController;
 use \Youkok2\Utilities\CacheManager as CacheManager;
@@ -42,20 +43,6 @@ class CourseDownloadUpdater extends Youkok2 {
         // For returning content
         $collection = [];
         
-        // Load the courses
-        $get_all_downloaded_data  = "SELECT a.id, a.parent" . PHP_EOL;
-        $get_all_downloaded_data .= "FROM archive a" . PHP_EOL;
-        $get_all_downloaded_data .= "WHERE a.is_visible = 1";
-        
-        $get_all_downloaded_data_query = Database::$db->prepare($get_all_downloaded_data);
-        $get_all_downloaded_data_query->execute();
-        while ($row = $get_all_downloaded_data_query->fetch(\PDO::FETCH_ASSOC)) {
-            $collection['element_' . $row['id']] = [
-                'id' => $row['id'],
-                'downloaded' => 0,
-                'parent' => $row['parent']];
-        }
-        
         // Get the downloaded information
         $get_downloads_in_period  = "SELECT d.file as 'id', COUNT(d.id) as 'downloaded_times'" . PHP_EOL;
         $get_downloads_in_period .= "FROM download d" . PHP_EOL;
@@ -69,56 +56,26 @@ class CourseDownloadUpdater extends Youkok2 {
         $get_downloads_in_period_query->execute();
         while ($row = $get_downloads_in_period_query->fetch(\PDO::FETCH_ASSOC)) {
             // Apply the dowloaded information to the collection array
-            if (isset($collection['element_' . $row['id']])) {
-                $collection['element_' . $row['id']]['downloaded'] = $row['downloaded_times'];
-            }
-        }
-        
-        
-        // Propagate the values to the parents
-        while (true) {
-
-            $found_child = false;
-            // Loop the collection
-            foreach ($collection as $k => $v) {
-                // Only handle children
-                if ($v['parent'] != null) {
-                    // Check if this has a downloaded value
-                    if ($v['downloaded'] > 0) {
-                        $found_child = true;
-                    }
-                    
-                    // Increase the number of downloades to the parent
-                    if (isset($collection['element_' . $v['parent']])) {
-                        $collection['element_' . $v['parent']]['downloaded'] += $v['downloaded'];
-                    }
-                    
-                    // Reset own downloaded
-                    $collection[$k]['downloaded'] = 0;
-                }
-            }
+            $element = new Element($row['id']);
+            $parent = $element->getRootParent();
             
-            // Check if we should break the loop
-            if (!$found_child) {
-                break;
-            }
-        }
-        
-        // Prettify the results
-        $collection_clean = [];
-        foreach ($collection as $k => $v) {
-            if ($v['downloaded'] > 0) {
-                $collection_clean[$v['id']] = $v['downloaded'];
+            if ($parent->wasFound()) {
+                if (isset($collection[$parent->getId()])) {
+                    $collection[$parent->getId()] += $row['downloaded_times'];
+                }
+                else {
+                    $collection[$parent->getId()] = $row['downloaded_times'];
+                }
             }
         }
         
         // Sort the result
-        arsort($collection_clean);
+        arsort($collection);
         
         // Get the 15 best results (if any)
         $result_array = [];
         $i = 0;
-        foreach ($collection_clean as $k => $v) {
+        foreach ($collection as $k => $v) {
             // Add to result array
             $result_array[] = [
                 'id' => $k,
