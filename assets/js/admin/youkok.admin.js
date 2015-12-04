cb806011bbb45f5153022d62d81743b2c2d94617
OptimusCrime <thomasgautv@hotmail.com>
2015-12-04 23:41:42 +0100

    Boilerplate for admin js file

---
+++ assets/js/admin/youkok.admin.js
@@ -0,0 +1,29 @@
+/*
+ * The Youkok2 admin module
+ */
+
+var Youkok2Admin = (function () {
+    
+    /*
+     * Public methods
+     */
+    
+    return {
+        
+        /*
+         * Init the module
+         */
+        
+        init = function () {
+            console.log('inited');
+        };
+    };
+})();
+
+/*
+ * jQuery goes here
+ */
+
+$(document).ready(function () {
+    Youkok2Admin.init();
+});
