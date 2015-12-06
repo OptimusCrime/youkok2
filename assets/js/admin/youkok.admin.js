b2df1af387bed754ec665dcee60c3af69e31ed7d
OptimusCrime <thomasgautv@hotmail.com>
2015-12-07 00:10:45 +0100

    Reimplement submenu functionality

--- assets/js/admin/youkok.admin.js
+++ assets/js/admin/youkok.admin.js
@@ -9,7 +9,7 @@ var Youkok2Admin = (function () {
      */
     
     var applyEventListeners = function () {
-        // Collaps/Expand
+        // Collaps/Expand modules
         $('[data-widget="collapse"]').on('click', function (e) {
             e.preventDefault();
             
@@ -21,6 +21,52 @@ var Youkok2Admin = (function () {
                     $(this).stop().slideDown();
                 }
             });
+        });
+        
+        // Collaps/Expand sidebar items
+        $('.treeview a').on('click', function (e) {
+            var $obj = $(this);
+            
+            // Make sure to only fire on sidebar expand/collaps elements
+            if ($obj.attr('href') == '#') {
+                e.preventDefault();
+                
+                // Some stuff
+                var $parent = $obj.parent();
+                var $grand_parent = $parent.parent();
+                var $submenu = $parent.find('ul').eq(0);
+                
+                // Make sure we have a submenu
+                if ($submenu) {
+                    // Check if we should expand or collaps
+                    if ($submenu.is(':visible')) {
+                        // Collaps child submenu
+                        $submenu.stop().slideUp(400, function () {
+                            // Remove active class
+                            $parent.removeClass('active');
+                            
+                            // Collaps all inner submenus too
+                            $(this).find('ul').hide();
+                            $(this).find('li.active').removeClass('active');
+                        });
+                    }
+                    else {
+                        // Collaps already expanded submenus
+                        $grand_parent.find('.treeview.active > ul:visible').stop().slideUp(400, function () {
+                            // Remove active class
+                            $(this).parent().removeClass('active');
+                            
+                            // Collaps all inner submenus too
+                            $(this).parent().find('ul').hide();
+                            $(this).parent().find('li.active').removeClass('active');
+                        });
+                        
+                        $submenu.stop().slideDown(400, function () {
+                            $parent.addClass('active');
+                        });
+                    }
+                }
+            }
         });
     };
     
