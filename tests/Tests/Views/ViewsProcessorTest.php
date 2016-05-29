789a734cf309080f96dad0ed4e11635a646b9d32
OptimusCrime <thomasgautv@hotmail.com>
2016-05-29 23:21:19 +0200

    Add test for processor view

---
+++ tests/Tests/Views/ViewsProcessorTest.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * File: ViewsProcessorTest.php
+ * Holds: Testes the Processor view
+ * Created: 29.05.2016
+ * Project: Youkok2
+ *
+ */
+
+namespace Youkok2\Tests\Views;
+
+use Youkok2\Youkok2;
+use Youkok2\Utilities\ClassParser;
+
+class ViewsProcessorTest extends \Youkok2\Tests\YoukokTestCase
+{
+    public function testProcessorView() {
+        $processor_wrapper = new Youkok2();
+        $processor_wrapper->load('processor/foobar', [
+            'close_db' => false,
+            'application' => true
+        ]);
+        $this->assertEquals('application/json', $processor_wrapper->getHeader('Content-Type'));
+
+    }
+
+}
