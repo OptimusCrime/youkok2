db597882b32692e99e8c13aa3bc1559cce4bea5e
OptimusCrime <thomasgautv@hotmail.com>
2016-10-06 01:09:40 +0200

    Add phpmd and tweak scripts

--- _scripts/tests.sh
+++ _scripts/tests.sh
@@ -7,9 +7,10 @@ php vendor/bin/phpcs
 if [[ "$(php -m)" == *"xdebug"* ]]
 then
     # We have xdebug, run with coverage
-    php vendor/bin/phpunit --coverage-clover clover.xml
-    php vendor/bin/coveralls --coverage_clover=clover.xml -v
+    php composer.phar phpunit --coverage-clover clover.xml
+    php composer.phar coveralls --coverage_clover=clover.xml -v
+    php composer.phar phpmd ./src xml cleancode,codesize,controversial,design,naming,unusedcode
 else
     # We do not have xdebug, just run phpunit
-    php vendor/bin/phpunit
+    php composer.phar phpunit
 fi
