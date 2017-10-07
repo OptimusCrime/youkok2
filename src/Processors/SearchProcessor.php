a0e446184b8b72aa5a86ecdaab23a678042c7263
Thomas Gautvedt <thomasgautv@hotmail.com>
2017-10-07 19:43:17 +0200

    Best search mechanic in the world

--- src/Processors/SearchProcessor.php
+++ src/Processors/SearchProcessor.php
@@ -9,6 +9,117 @@ class SearchProcessor
 {
     public static function run($query = null)
     {
-        return ElementController::getElementsFromSearchQuery($query);
+        return ElementController::getElementsFromSearchQuery(static::cleanSearch($query));
     }
+
+    private static function cleanSearch($search)
+    {
+        // TODO upper limit for number of sub queries to avoid crashing everything
+
+        $splitQuery = static::splitSearch($search);
+        $splitQuerySQL = static::translateToSQL($splitQuery);
+
+        $permutations = static::buildQueryPermutations($splitQuerySQL);
+        var_dump($permutations);
+        die();
+    }
+
+    private static function splitSearch($search)
+    {
+        if (strpos($search, ' ') === false) {
+            return [$search];
+        }
+
+        $searchSplit = explode(' ', $search);
+        $searchClean = [];
+        foreach ($searchSplit as $v) {
+            if (strlen($v) > 0) {
+                $searchClean[] = $v;
+            }
+        }
+
+        return $searchClean;
+    }
+
+    private static function translateToSQL(array $search)
+    {
+        $newSearch = [];
+        foreach ($search as $v) {
+            $newSearch[] = str_replace('*', '%', $v);
+        }
+
+        return $newSearch;
+    }
+
+    private static function buildQueryPermutations(array $search)
+    {
+        if (count($search) === 1) {
+            return [
+                $search[0] . '||%',
+                '%||' . $search[0]
+            ];
+        }
+
+        $query = [];
+        $permutations = static::pc_permute($search);
+        foreach ($permutations as $v) {
+            // Partition the array. First result should have no values on the left side, and all on the right,
+            // second should have one values on the left side, and the remaining right, ...
+            $partitions = static::partitionPermutations($v);
+            foreach ($partitions as $par) {
+                // TODO
+            }
+        }
+
+        //var_dump($query);
+
+        die();
+    }
+
+    private static function partitionPermutations(array $values)
+    {
+        $partitions = [];
+        for ($i = 0; $i < (count($values) + 1); $i++) {
+            $partitions[] = static::partitionArray($values, $i);
+        }
+
+        return $partitions;
+    }
+
+    private static function partitionArray(array $values, $leftSide)
+    {
+        // TODO write description here
+        $left = [];
+        $right = [];
+        for ($i = 0; $i < $leftSide; $i++) {
+            $left[] = $values[$i];
+        }
+        for ($i = $leftSide; $i < count($values); $i++) {
+            $right[] = $values[$i];
+        }
+
+        return [
+            'left' => $left,
+            'right' => $right
+        ];
+    }
+
+    // TODO split out and clean up
+    private static function pc_permute($items, $perms = array( )) {
+        if (empty($items)) {
+            return [$perms];
+        }
+
+        $return = [];
+        for ($i = count($items) - 1; $i >= 0; --$i) {
+            $newitems = $items;
+            $newperms = $perms;
+            list($foo) = array_splice($newitems, $i, 1);
+            array_unshift($newperms, $foo);
+            $return = array_merge($return, static::pc_permute($newitems, $newperms));
+        }
+
+        return $return;
+    }
+
 }
