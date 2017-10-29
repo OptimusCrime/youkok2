0383bfa89cc5448da65e71ca2ec03260fdc19e63
OptimusCrime <thomasgautv@hotmail.com>
2017-10-29 18:39:39 +0100

    Remove old files

--- old_files/src/Youkok2/Models/Cache/CourseDownloads.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Youkok2\Models\Cache;
-
-class CourseDownloads extends CacheModel
-{
-    protected $controllerClass = \Youkok2\Models\Controllers\Cache\CourseDownloadsController::class;
-}
