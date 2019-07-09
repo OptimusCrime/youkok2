4e7c2203209c0cf72488e9f3caa1d1ad91d028e3
OptimusCrime <thomasgautv@hotmail.com>
2019-07-09 02:53:18 +0200

    Litt kjapp på avtrekkeren der

--- frontend/src/sidebarPost/utilities.js
+++ frontend/src/sidebarPost/utilities.js
@@ -1,9 +1,9 @@
 // ... https://stackoverflow.com/a/5717133/921563
 const validUrlPattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+  '((([a-zæøå\\d]([a-zæøå\\d-]*[a-zæøå\\d])*)\\.)+[a-zæøå]{2,}|'+ // domain name
   '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-  '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  '(\\:\\d+)?(\\/[-a-zæøå\\d%_.~+]*)*'+ // port and path
+  '(\\?[;&a-zæøå\\d%_.~+=-]*)?'+ // query string
+  '(\\#[-a-zæøå\\d_]*)?$','i'); // fragment locator
 
 export const isValidUrl = str => !!validUrlPattern.test(str);
